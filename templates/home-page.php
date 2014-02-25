a7d832f0f675692be13694a962c31ec363b55ee4
Devin Price <devin@wptheming.com>
2014-02-24 21:03:39 -0600

    Issue #32.  Update markup for homepage template.

--- templates/home-page.php
+++ templates/home-page.php
@@ -13,14 +13,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php the_content(); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
 
