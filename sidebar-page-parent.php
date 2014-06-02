b0e96c9f3b7016b6db59cbf6b62ef2cbf77ae93f
Devin Price <devin@wptheming.com>
2014-06-02 09:44:06 -0500

    Update sidebar-page-parent.php
    Issue #52.  Markup bug.

--- sidebar-page-parent.php
+++ sidebar-page-parent.php
@@ -8,7 +8,7 @@
 	<div id="secondary" class="widget-area" role="complementary">
 
 		<aside class="widget widget_pages">
-			<h3 class="widget-title"><?php _e( 'Pages', 'govpress' ); ?></h1>
+			<h1 class="widget-title"><?php _e( 'Pages', 'govpress' ); ?></h1>
 			<ul>
 				<?php wp_list_pages( array(
 					'title_li' => '',
