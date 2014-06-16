b5e07ec6248e5c26e1bb1c5ed6b2086cf4e5cffd
Ethan Teague <metrowebz@gmail.com>
2014-06-16 08:59:25 -0400

    #34 - Changed logic to acct. for high volume / lots of requests - additional check for server response time. 404.php

--- 404.php
+++ 404.php
@@ -19,36 +19,41 @@ get_header(); ?>
    
 
 <?php
+
+$usage = getrusage();
     
-$search_val = $wp_query->query_vars['name'];
-$args = array( 'posts_per_page' => 1000, 'order' => 'ASC', 'hide_empty' => 1, 'post_type' => array('page','post') );
+if ( $usage['ru_stime.tv_usec'] < 100000 ) {
+    $search_val = $wp_query->query_vars['name'];
+    $args = array( 'posts_per_page' => 1000, 'order' => 'ASC', 'hide_empty' => 1, 'post_type' => array('page','post') );
 
-$posts_raw = new WP_Query;
-$posts = $posts_raw->query($args);
+    $posts_raw = new WP_Query;
+    $posts = $posts_raw->query($args);
 
-if ( !empty( $posts ) ) {
-    echo '<h4>Are you looking for something like this?</h4>';
-}
+    if ( !empty( $posts ) ) {
+        echo '<h4>Are you looking for something like this?</h4>';
+    }
 
-foreach($posts as $post) {
-	similar_text( $search_val, $post->post_name, $percent );
-	if ( $percent > 40 ) {
-	    echo '<a href="'.get_permalink( $post->ID ).'">'.$post->post_title.'</a><br />';
-	}
+    foreach($posts as $post) {
+        similar_text( $search_val, $post->post_name, $percent );
+        if ( $percent > 40 ) {
+            echo '<a href="'.get_permalink( $post->ID ).'">'.$post->post_title.'</a><br />';
+        }
+    }
 }
 
 $i = 0;
 $category_ids = get_all_category_ids();
 foreach ($category_ids as $cat_id) {
-	$cat_name = get_cat_name( $cat_id);
-	if ( $i < 1 ) {
-		echo '<hr />';
-		echo 'Browse by category:<br />';
-	}
-	echo '<a href="'.get_category_link( $cat_id ).'">'.$cat_name.'</a><br />';
-	$i++;
+    $cat_name = get_cat_name( $cat_id);
+    if ( $i < 1 ) {
+        echo '<hr />';
+        echo 'Browse by category:<br />';
+    }
+    echo '<a href="'.get_category_link( $cat_id ).'">'.$cat_name.'</a><br />';
+    $i++;
 }
 
+    
 ?>
 
 
