9440b6a6f6a6ec9cfb7c0cfb252c8f140c4c25a9
Devin Price <devin@wptheming.com>
2015-12-27 13:27:17 -0600

    #100 Remove compatibility view for IE.

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=10">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <!--[if lt IE 9]>
